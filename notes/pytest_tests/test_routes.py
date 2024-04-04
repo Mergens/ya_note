f66dc22de8f870813d056ecb7f6ccfb64d2544eb
Mergens <smergen@gmail.com>
2024-04-04 16:43:58 +0300

    4

--- notes/pytest_tests/test_routes.py
+++ notes/pytest_tests/test_routes.py
@@ -83,19 +83,19 @@ def test_redirects(client, name, args):
     assertRedirects(response, expected_url)
 
 
-# def test_note_exists(note):
-#     notes_count = Note.objects.count()
-#     # Общее количество заметок в БД равно 1.
-#     assert notes_count == 1
-#     # Заголовок объекта, полученного при помощи фикстуры note,
-#     # совпадает с тем, что указан в фикстуре.
-#     assert note.title == 'Заголовок'
+def test_note_exists(note):
+    notes_count = Note.objects.count()
+    # Общее количество заметок в БД равно 1.
+    assert notes_count == 1
+    # Заголовок объекта, полученного при помощи фикстуры note,
+    # совпадает с тем, что указан в фикстуре.
+    assert note.title == 'Заголовок'
 
 
-# # Обозначаем, что тесту нужен доступ к БД.
-# # Без этой метки тест выдаст ошибку доступа к БД.
-# @pytest.mark.django_db
-# def test_empty_db():
-#     notes_count = Note.objects.count()
-#     # В пустой БД никаких заметок не будет:
-#     assert notes_count == 0
+# Обозначаем, что тесту нужен доступ к БД.
+# Без этой метки тест выдаст ошибку доступа к БД.
+@pytest.mark.django_db
+def test_empty_db():
+    notes_count = Note.objects.count()
+    # В пустой БД никаких заметок не будет:
+    assert notes_count == 0
